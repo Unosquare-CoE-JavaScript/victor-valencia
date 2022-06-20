8fada45c04ddc27ca2898a75c71fc253a32e8502
Victor Valencia <vvalencia@nonstophr.com>
2022-06-19 22:03:00 -0500

    addition of the finally method to the async function calling

--- asynchronous-javascript-deep-dive/micro-macro-tasks/js/app.js
+++ asynchronous-javascript-deep-dive/micro-macro-tasks/js/app.js
@@ -1,27 +1,6 @@
 "use strict";
 
-const massiveProcess = function(num) {
-    return new Promise(function(resolve, reject) {
-        if (isNaN(num)) {
-            reject("Please enter a number!");
-        } else {
-            let result = 0;
-            setTimeout(function() {
-                for (let i = num ** 7; i >= 0; i--) {        
-                    result += Math.atan(i) * Math.tan(i);
-                };
-                resolve(result);
-            }, 0);
-        }
-    });
-};
-
-massiveProcess(10)
-    .then(function(amt) {
-        console.log("The number is: " + amt);
-    })
-    .catch(function(msg) {
-        console.error(msg);
-    });
-
-console.log(5 * 5 + 100);
+asyncFunction2()
+.then(msg => console.log(msg))
+.catch(err => console.log(err))
+.finally(() => console.log("Cleaning up tasks."));
