1f72db91799201f66ab7d66d8df6197b47a4b65d
Victor Valencia <vvalencia@nonstophr.com>
2022-06-19 22:20:11 -0500

    implementation of the allsettled and any static methods for promises to api

--- asynchronous-javascript-deep-dive/requesting-async/js/app.js
+++ asynchronous-javascript-deep-dive/requesting-async/js/app.js
@@ -5,14 +5,12 @@ var MAINAPP = (function(namespace) {
 
     let p1 = fetch(url + 'posts/').then(response1 => response1.json()),
         p2 = fetch(url + 'comments/').then(response2 => response2.json()),
-        p3 = fetch(url + 'todos/').then(response3 => response3.json());
+        p3 = fetch(url + 'todos/').then(response3 => response3.json()),
+        p4 = Promise.reject('Testing Static Methods');
 
-    Promise.all([p1, p2, p3])
-    .then(msg => {
-        namespace.posts = msg[0];
-        namespace.comments = msg[1];
-        namespace.todos = msg[2];
-        console.log("We have received the data!");
+    Promise.any([p1, p2, p3, p4])
+    .then(results => {
+        console.log(results);
     })
     .catch(err => console.log(`Problem retrieving data: ${err}`));
 
