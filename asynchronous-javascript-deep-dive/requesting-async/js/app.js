9962d207380ed6a9041729241b6e1c6875450911
Victor Valencia <vvalencia@nonstophr.com>
2022-06-19 22:17:57 -0500

    usage of the async requesting by adding the all method for the data fetching from api

--- asynchronous-javascript-deep-dive/requesting-async/js/app.js
+++ asynchronous-javascript-deep-dive/requesting-async/js/app.js
@@ -3,20 +3,18 @@ var MAINAPP = (function(namespace) {
 
     let url = 'https://jsonplaceholder.typicode.com/';
 
-    fetch(url + 'posts/')
-    .then(response1 => response1.json())
-    .then(posts => namespace.posts = posts)
-    .catch(err => console.log(`Problem retrieving posts: ${err}`));
+    let p1 = fetch(url + 'posts/').then(response1 => response1.json()),
+        p2 = fetch(url + 'comments/').then(response2 => response2.json()),
+        p3 = fetch(url + 'todos/').then(response3 => response3.json());
 
-    fetch(url + 'comments/')
-    .then(response2 => response2.json())
-    .then(comments => namespace.comments = comments)
-    .catch(err => console.log(`Problem retrieving comments: ${err}`));
-
-    fetch(url + 'todos/')
-    .then(response3 => response3.json())
-    .then(todos => namespace.todos = todos)
-    .catch(err => console.log(`Problem retrieving todos: ${err}`));
+    Promise.all([p1, p2, p3])
+    .then(msg => {
+        namespace.posts = msg[0];
+        namespace.comments = msg[1];
+        namespace.todos = msg[2];
+        console.log("We have received the data!");
+    })
+    .catch(err => console.log(`Problem retrieving data: ${err}`));
 
     return namespace;
 })(MAINAPP || {});
