d15e761a42732ec9b40f4b6248ea0b4baba95a79
Victor Valencia <vvalencia@nonstophr.com>
2022-06-25 23:05:08 -0500

    second implementation of async await to jsonplaceholder api

--- asynchronous-javascript-deep-dive/promises-jsonplaceholder/js/app.js
+++ asynchronous-javascript-deep-dive/promises-jsonplaceholder/js/app.js
@@ -4,13 +4,13 @@
 .then(data => data.json())
 .then(obj => console.log(obj));*/
 
-/*let todo = {
+let todo = {
     completed: false,
     userId: 1,
     title: "Learn Promises"
 };
 
-fetch('https://sonplaceholder.typicode.com/todos/', {
+/*fetch('https://sonplaceholder.typicode.com/todos/', {
     method: 'POST',
     headers: {
         "Content-type": "application/json"
@@ -21,11 +21,27 @@ fetch('https://sonplaceholder.typicode.com/todos/', {
 .then(obj => console.log(obj))
 .catch(reject => console.log(`Unable to create todo ${reject}`));*/
 
-(async function() {
-    let data=  await fetch('https://jsonplaceholder.typicode.com/todos/');
+const asyncFunction = async() => {
+    try{
+        let data = await fetch('https://jsonplaceholder.typicode.com/todos/', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(todo)
+        });
+        console.log(await data.json());
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+/*(async function() {
+    let data = await fetch('https://jsonplaceholder.typicode.com/todos/');
 
     let res = await data.json();
     console.log(res);
-})();
+})();*/
 
+asyncFunction();
 console.log('Other code');
