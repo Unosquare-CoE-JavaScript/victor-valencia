94346e414d0a8c37fb46567dc65b55a5d8714b06
Victor Valencia <vvalencia@nonstophr.com>
2022-06-25 23:40:23 -0500

    using async await for get posts from jsonplaceholder api

--- asynchronous-javascript-deep-dive/requesting-async/js/app.js
+++ asynchronous-javascript-deep-dive/requesting-async/js/app.js
@@ -3,7 +3,17 @@ var MAINAPP = (function(namespace) {
 
     let url = 'https://jsonplaceholder.typicode.com/';
 
-    let p1 = fetch(url + 'posts/').then(response1 => response1.json()),
+    (async function() {
+        try {
+            let data = await fetch(url + 'posts/'),
+            posts = await data.json();
+            namespace.posts = posts;
+        } catch(e) {
+            console.log(`Problem retrieving data: ${err}`);
+        }
+    })();
+
+    /*let p1 = fetch(url + 'posts/').then(response1 => response1.json()),
         p2 = fetch(url + 'comments/').then(response2 => response2.json()),
         p3 = fetch(url + 'todos/').then(response3 => response3.json()),
         p4 = Promise.reject('Testing Static Methods');
@@ -12,7 +22,7 @@ var MAINAPP = (function(namespace) {
     .then(results => {
         console.log(results);
     })
-    .catch(err => console.log(`Problem retrieving data: ${err}`));
+    .catch(err => console.log(`Problem retrieving data: ${err}`));*/
 
     return namespace;
 })(MAINAPP || {});
