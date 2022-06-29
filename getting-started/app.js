3d9e7d417687e800360f2aee208911ac8db484f5
Victor Valencia <vvalencia@nonstophr.com>
2022-06-28 21:17:49 -0500

    Values are introduced and how the data can be evaluated

---
+++ getting-started/app.js
@@ -0,0 +1,44 @@
+"use strict";
+// ==================Values=====================
+
+var firstName = "Victoria";
+
+console.log("My name is '${ firstName }'.");
+console.log('My name is', firstName);
+console.log(`My name is ${firstName}.`);
+
+while (false) {
+    console.log(3.141592);
+}
+
+var names = ["Laura", "Monica", "Patrick"]
+
+console.log(`My name is ${names[1]}.`);
+
+var a;
+
+if (a === undefined) {
+    console.log('a is undefined');
+}
+
+if (a === null) {
+    console.log('a is null');
+}
+
+
+var obj = {
+    first: "Kyle",
+    last: "Simpson",
+    age: 39,
+    specialties: ["JS", "Table Tennis"]
+};
+
+console.log(`My name is ${obj.first}.`);
+console.log(`My name is ${obj['first']}.`);
+
+var array = [42, "abc", true, undefined, null, { "a": 1 }, [1, 2, 3], function hello() { }];
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(typeof element);
+}
